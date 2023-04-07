0f328c5dcb37f5b831a076cd19518bf5b337f749
RutujaMogal <98806903+RutujaMogal@users.noreply.github.com>
2023-04-07 04:43:22 -0700

    Add files via upload

---
+++ TreeSetExp.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.TreeSet;
+import java.util.Set;
+
+public class TreeSetExp {
+
+	public static void main(String[] args) {
+		Set<String> s=new TreeSet<String>();
+		//adding element into set
+		s.add("cat");
+		s.add("ant");
+		s.add("apple");
+		s.add("dog");
+		
+		
+		
+		System.out.println("The elemnt is:"+s);
+	}
+
+}
